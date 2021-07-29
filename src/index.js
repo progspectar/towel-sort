b5ea2acb82a630bb2379d3e4a5a05954b8333f0b
progspectar <progspectar@gmail.com>
2021-07-29 12:50:08 +0700

    feat: implementation task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+function towelSort(matrix) {
+    if (!matrix) return [];
+
+    const arr = matrix.map((subarr, idx) => {
+        return idx % 2 === 0
+            ? subarr.sort((a, b) => a - b)
+            : subarr.sort((a, b) => b - a);
+    });
+    return arr.flat();
 }
+
+module.exports = towelSort;
